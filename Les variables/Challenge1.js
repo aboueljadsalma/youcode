f5ffbd20f85d470412e6202ca304c6c2702bd16b
salma aboueljad <aboueljadsalma@gmail.com>
2026-09-18 09:17:14 +0100

    sec

---
+++ Les variables/Challenge1.js
@@ -0,0 +1,6 @@
+const prompt = require('prompt-sync')();
+let nom = prompt ('quel est ton nom?')
+let prenom = prompt ('quel est ton prenom?')
+let age = prompt ('quel est votre age?')
+let e_mail = prompt ('entre votre adresse e-mail')
+console.log("nom "+nom,"prenom "+prenom,"age "+age,"e_mail "+e_mail)
