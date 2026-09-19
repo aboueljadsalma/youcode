a389a1e11a1cf13b1093c159a1064d71ea7aa99b
aboueljadsalma <aboueljadsalma@gmail.com>
2026-09-19 11:58:48 +0100

    Delete Les variables directory

--- Les variables/Challenge1.js
+++
@@ -1,6 +0,0 @@
-const prompt = require('prompt-sync')();
-let nom = prompt ('quel est ton nom?')
-let prenom = prompt ('quel est ton prenom?')
-let age = prompt ('quel est votre age?')
-let e_mail = prompt ('entre votre adresse e-mail')
-console.log("nom "+nom,"prenom "+prenom,"age "+age,"e_mail "+e_mail)
--- Les variables/Challenge2.js
+++
@@ -1,4 +0,0 @@
-const prompt = require('prompt-sync')();
-let celsius = prompt('donnez la tenpérature de celsius:')
-let kelvin = Number(celsius) + 273.15;
-console.log("la température en Kelvin c'est : " + kelvin);
